c35b7f371de25bf8145542f7ff37c6c1236813d7
Vitek11 <106838153+Vitek11@users.noreply.github.com>
2022-06-15 09:16:23 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -10,18 +10,19 @@ func main() {
 		money      = 23.
 		numApple   = 9
 		numPear    = 8
-		sum1       float64
+		totalCost  float64
 	)
 
 	fmt.Println("Apple price:", applePrice, "grn")
 	fmt.Println("Pear price:", pearPrice, "grn")
 	fmt.Println("We have:", money, "grn")
 
-	sum1 = float64(numApple)*applePrice + float64(numPear)*pearPrice
-	fmt.Println("1. To buy", numApple, "apples and", numPear, "pears you need", sum1)
+	totalCost = float64(numApple)*applePrice + float64(numPear)*pearPrice
+	fmt.Println("1. To buy", numApple, "apples and", numPear, "pears you need", totalCost)
 	fmt.Println("2. We can buy", int32(money/applePrice), "apples")
 	fmt.Println("3. We can buy", int32(money/pearPrice), "pears")
 	numApple = 2
 	numPear = 2
-	fmt.Println("4. Can we buy", numApple, "pears and", numPear, "apples?", float64(numApple)*applePrice+float64(numPear)*pearPrice < money)
+	totalCost = float64(numApple)*applePrice + float64(numPear)*pearPrice
+	fmt.Println("4. Can we buy", numApple, "apples and", numPear, "pears?", totalCost < money)
 }
